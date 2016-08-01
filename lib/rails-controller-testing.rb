19643eaf7b0673b5cf931198dbf81c60247cc967
Akira Matsuda <ronnie@dio.jp>
2016-08-01 20:28:25 +0900

    No need to #send a public method

--- lib/rails-controller-testing.rb
+++ lib/rails-controller-testing.rb
@@ -5,14 +5,14 @@ require 'rails/controller/testing/template_assertions'
 require 'rails/controller/testing/version'
 
 ActiveSupport.on_load(:action_controller) do
-  ActionController::TestCase.send(:include, Rails::Controller::Testing::TestProcess)
-  ActionController::TestCase.send(:include, Rails::Controller::Testing::TemplateAssertions)
+  ActionController::TestCase.include Rails::Controller::Testing::TestProcess
+  ActionController::TestCase.include Rails::Controller::Testing::TemplateAssertions
 
-  ActionDispatch::IntegrationTest.send(:include, Rails::Controller::Testing::TemplateAssertions)
-  ActionDispatch::IntegrationTest.send(:include, Rails::Controller::Testing::Integration)
-  ActionDispatch::IntegrationTest.send(:include, Rails::Controller::Testing::TestProcess)
+  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TemplateAssertions
+  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::Integration
+  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TestProcess
 end
 
 ActiveSupport.on_load(:action_view) do
-  ActionView::TestCase.send(:include, Rails::Controller::Testing::TemplateAssertions)
+  ActionView::TestCase.include Rails::Controller::Testing::TemplateAssertions
 end
