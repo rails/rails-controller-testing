e56cee404a67a812514e95f92282632ada1912e9
Sean Griffin <sean@thoughtbot.com>
2016-08-11 14:38:05 -0400

    Ensure that our load hooks really for real run after Rails
    While changing the code to be in an `on_load` hook fixed some of the issues
    with load hook order, it didn't catch all of them. Rails runs its hooks in
    initializers, meaning they'll be defined after our own hooks which are not in
    an initializer.
    
    Fixes #24

--- lib/rails-controller-testing.rb
+++ lib/rails-controller-testing.rb
@@ -4,15 +4,19 @@ require 'rails/controller/testing/integration'
 require 'rails/controller/testing/template_assertions'
 require 'rails/controller/testing/version'
 
-ActiveSupport.on_load(:action_controller) do
-  ActionController::TestCase.include Rails::Controller::Testing::TestProcess
-  ActionController::TestCase.include Rails::Controller::Testing::TemplateAssertions
+class Rails::Controller::Testing::Railtie < Rails::Railtie
+  initializer "rails_controller_testing" do
+    ActiveSupport.on_load(:action_controller) do
+      ActionController::TestCase.include Rails::Controller::Testing::TestProcess
+      ActionController::TestCase.include Rails::Controller::Testing::TemplateAssertions
 
-  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TemplateAssertions
-  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::Integration
-  ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TestProcess
-end
+      ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TemplateAssertions
+      ActionDispatch::IntegrationTest.include Rails::Controller::Testing::Integration
+      ActionDispatch::IntegrationTest.include Rails::Controller::Testing::TestProcess
+    end
 
-ActiveSupport.on_load(:action_view) do
-  ActionView::TestCase.include Rails::Controller::Testing::TemplateAssertions
+    ActiveSupport.on_load(:action_view) do
+      ActionView::TestCase.include Rails::Controller::Testing::TemplateAssertions
+    end
+  end
 end
