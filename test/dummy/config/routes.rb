523a4090c9f720fa9277e8be32d0ed8aee98af76
Karl Johansson <karl.johansson@t2data.com>
2019-08-27 10:56:58 +0200

    Avoid deprecated dynamic controller/action segment
    The dynamic :controller and :action segments are removed from the route
    configurations in Rails 6.1.
    
    This commit updates the routing configuration for the tests to avoid the
    deprecation warnings while still generating routes dynamically.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get ':controller(/:action)'
+  ActionController::Base.descendants.each do |controller|
+    controller.action_methods.each do |action|
+      get "#{controller.controller_name}/#{action}"
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
